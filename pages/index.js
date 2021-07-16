99604c9e78bb8e2d09395e13d06b03864f1b8a3f
anungbisa4 <anung.bisa4@gmail.com>
2021-07-16 12:22:15 +0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = (props) => {
     <>
       <main>
         <div className="flex w-full h-screen justify-center items-center flex-col space-y-7">
-          <Image src="/images/logo.png" layout="fixed" width={192} height={192 }/>
+          <Image src="/images/logo.png" layout="fixed" width={192} height={192 } alt="nice logo"/>
           <h1 className="font-bold text-2xl">Pensilpatah next boilerplate</h1>
           <h2>Env : {process.env.baseUrl}</h2>
           <p>Created By: Anung Aninditha</p>
