8fe532929e78fd38b170d5fe830d52426c18f572
anungbisa4 <anung.bisa4@gmail.com>
2021-07-16 13:54:17 +0700

    fix amp file

--- pages/examples/sample-amp.js
+++ pages/examples/sample-amp.js
@@ -1,14 +1,24 @@
 export const config = { amp: true };
 
-function SampleAMP(props) {
+function SampleAMP() {
   return (
-    <amp-img
-      width="300"
-      height="300"
-      src="/images/logo.png"
-      alt="a cool image"
-      layout="responsive"
-    />
+    <div>
+      <p>
+        Muffin gummies dessert cheesecake candy canes. Candy canes danish cotton
+        candy tart dessert powder bear claw marshmallow. Muffin chocolate
+        marshmallow danish. Chocolate bar biscuit cake tiramisu. Topping sweet
+        brownie jujubes powder marzipan. Croissant wafer bonbon chupa chups cake
+        cake marzipan caramels jujubes. Cupcake cheesecake sweet roll
+        marshmallow lollipop danish jujubes jelly icing. Apple pie chupa chups
+        lollipop jelly-o cheesecake jelly beans cake dessert. Tootsie roll
+        tootsie roll bonbon pastry croissant gummi bears cake cake. Fruitcake
+        sugar plum halvah gingerbread cookie pastry chupa chups wafer lemon
+        drops. Marshmallow liquorice oat cake lollipop. Lemon drops oat cake
+        halvah liquorice danish powder cupcake soufflé. Cake tart topping
+        jelly-o tart sugar plum. Chocolate bar cookie wafer tootsie roll candy
+        cotton candy toffee pie donut.
+      </p>
+    </div>
   );
 }
 
