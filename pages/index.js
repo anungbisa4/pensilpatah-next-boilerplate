fe52bc6ac58a91494cbaad2cedcf6c7be99db726
anungbisa4 <anung.bisa4@gmail.com>
2021-07-16 12:17:50 +0700

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import Head from "next/head";
 import dynamic from "next/dynamic";
+import Image from "next/image"
 
 const Home = (props) => {
   return (
     <>
-      <section>
-        <h1>Pensilpatah next boilerplate</h1>
-        <h2>Env : {process.env.baseUrl}</h2>
-      </section>
+      <main>
+        <div className="flex w-full h-screen justify-center items-center flex-col space-y-7">
+          <Image src="/images/logo.png" layout="fixed" width={192} height={192 }/>
+          <h1 className="font-bold text-2xl">Pensilpatah next boilerplate</h1>
+          <h2>Env : {process.env.baseUrl}</h2>
+          <p>Created By: Anung Aninditha</p>
+        </div>
+      </main>
     </>
   );
 };
