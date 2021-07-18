408bb882cc780a3f1a875801d266ac619fc3c40c
anungbisa4 <anung.bisa4@gmail.com>
2021-07-18 14:00:54 +0700

    add temp cdn

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
     // You can, for example, get the latest git commit hash here
     return "-dlingo-secret-1234";
   },
+  assetPrefix: "http://1147483289.rsc.cdn77.org",
   images: {
     domains: [],
   },
